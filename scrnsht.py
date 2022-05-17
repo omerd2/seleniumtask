5d7d2fb19ae9ce8517e64075526e6b14a706127c
omerd2 <72158259+omerd2@users.noreply.github.com>
2022-05-17 09:56:52 +0300

    Update scrnsht.py

--- scrnsht.py
+++ scrnsht.py
@@ -1,13 +1,22 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver import FirefoxOptions
+import argparse
+import shutil
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--url', action='store', dest='url', help='Insert the web URL.')
+args = parser.parse_args()
+
+URL = args.url
 
 opts = FirefoxOptions()
 opts.add_argument("--headless")
 browser = webdriver.Firefox(options=opts)
-browser.get('https://www.python.org')
+browser.get(URL)
 sleep(2)
 
 browser.get_screenshot_as_file("screenshot.png")
+shutil.move("/screenshot.png", "/screenshots/screenshot.png")
 browser.quit()
-print("end...")
+print("end..."
